cf7e79973cd970f0a136fed804be784a2cd5a2c7
Ahmed Amr <ahmed.amr2@vodafone.com>
2026-07-30 17:06:01 +0300

    Complete Rag with LLM

--- Script/main.py
+++ Script/main.py
@@ -2,6 +2,7 @@ from Embeddings import Embeddings
 from DataBase_Connection import Connection
 from Storage import Text_Storage
 from RAG import Retrieval
+from GEMINAI import LLM
 def run():
     Embedder = Embeddings()
     embedder = Embedder.get_embbder()
@@ -12,24 +13,24 @@ def run():
     store = Text_Storage(embedder, collection)
     print ("Store OK")
     retrieve = Retrieval(embedder, collection)
-
+    Model = LLM(embedder, collection)
+    print("LLM OK")
     
     print("Hello, How can I help you today?")
 
 
     while True:
-        user_input = input("YOU: ").lower().strip()
-        if user_input in ['exit', 'quit']:
+        user_input = input("YOU: ").strip()
+        if user_input.lower() in ['exit', 'quit']:
             break
         if user_input.startswith("info:"):
             store.store_text(user_input[6:].strip())
             print("El_3ariff: I got it. Thanks for the info")
         else:    
-            answer = retrieve.retrieve_chunks(user_input)
-            if not answer:
-                print("Assistant: I don't know...\n")
-            else:
-                print(f"Assistant: {"\n".join(answer)}\n")
+            retrieved_context = retrieve.retrieve_chunks(user_input)
+            answer = Model.ask_rag_system(user_input , retrieved_context)
+
+            print(f"Assistant: {answer}")
 
 
             
